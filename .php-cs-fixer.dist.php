e1f5095eabe8f6376f4f8850ce1ba48d12319b99
Antonio J. García Lagar <aj@garcialagar.es>
2025-06-26 08:24:38 +0200

    Fix LICENSE copyright notice
    Remove copyright notice from php files, too.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,16 +1,5 @@
 <?php
 
-$header = <<<'EOS'
-This file is part of the ajgl/feature-flag-bundle package.
-
-It has been borrowed from https://github.com/symfony/symfony/pull/53213.
-
-(c) Fabien Potencier <fabien@symfony.com>
-
-For the full copyright and license information, please view the LICENSE
-file that was distributed with this source code.
-EOS;
-
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
     ->exclude('tests/bin')
@@ -22,7 +11,7 @@ return (new PhpCsFixer\Config())
         '@Symfony' => true,
         '@Symfony:risky' => true,
         '@PHP81Migration' => true,
-        'header_comment' => ['header' => $header]
+        'header_comment' => ['header' => '']
     ])
     ->setFinder($finder)
 ;
